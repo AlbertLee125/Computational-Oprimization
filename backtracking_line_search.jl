48716ebabeda326cc8d92230ccfc635b26c2b7f3
Albert Lee <118876546+AlbertLee125@users.noreply.github.com>
2023-04-16 17:34:19 -0400

    determines the step size

---
+++ backtracking_line_search.jl
@@ -0,0 +1,27 @@
+function backtracking_line_search(A, b, c, x, lambda, s, dx, dlambda, ds, alpha, beta)
+    t = 1.0 #alpha = 0.01, beta = 0.5
+    m, n = size(A)
+
+    while true
+        x_new = x + t * dx
+        lambda_new = lambda + t * dlambda
+        s_new = s + t * ds
+
+        # Check if x_new and s_new are non-negative
+        if all(x_new .>= 0) && all(s_new .>= 0)  
+            r_p_new = A * x_new - b
+            r_d_new = A' * lambda_new + s_new - c
+
+            # Check the sufficient decrease condition
+            if norm(r_p_new) <= (1 - alpha * t) * norm(A * x - b) &&
+               norm(r_d_new) <= (1 - alpha * t) * norm(A' * lambda + s - c)
+                break
+            end
+        end
+
+        # Reduce the step size by the factor beta
+        t *= beta
+    end
+
+    return t
+end
