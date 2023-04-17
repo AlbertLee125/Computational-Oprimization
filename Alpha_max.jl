7ba7dd40651a03329a671b866d01efa38cd2bff4
TimeKP <102217751+TimeKP@users.noreply.github.com>
2023-04-17 12:38:28 -0400

    Add files via upload

---
+++ Alpha_max.jl
@@ -0,0 +1,17 @@
+function alpha_max(x, dx, hi = 1.0)
+    n = length(x)
+    alpha = hi
+    index = 0
+
+    for i=1:n
+         if dx[i] < 0.0
+              diff = -x[i]/dx[i]
+              if diff < alpha
+                   diff = alpha
+                   index = i
+              end
+        end
+    end
+
+    return alpha, index
+end
